6ed61d42850c14a94be87b37b9f1383117c5f975
Ceebox <chadderboxdev@gmail.com>
2025-07-25 15:41:17 +0100

    Add pause

--- src/scripts/automata.js
+++ src/scripts/automata.js
@@ -1,6 +1,7 @@
 import { Renderer } from "./modules/rendering/Renderer.mjs";
 
 const FPS = 4;
+let paused = true;
 let renderer = undefined;
 
 function run() {
@@ -10,31 +11,14 @@ function run() {
         return;
     }
 
+    // Produce the first frame (even though we are paused)
     renderer = new Renderer(canvas);
+    renderer.render();
 
     window.addEventListener("keydown", (key) => {
 
-        // I'm making this mostly offline, we're probably not going to have this    
-        if (typeof SPECTOR === "undefined") {
-            console.error("SPECTOR has not been loaded correctly.");
-            return;
-        }
-
-        if (key.key !== "/") {
-            return;
-        }
-
-        const spector = new SPECTOR.Spector();
-        spector.spyCanvases();
-        spector.displayUI();
-
-        renderer.render();
-
-        // Capture from canvas:
-        spector.captureCanvas(canvas).then((result) => {
-            const resultView = new SPECTOR.ResultView(spector);
-            resultView.display(result); // show the capture result
-        });
+        handlePause(key);
+        handleDebug(key);
     });
 
     requestAnimationFrame(() => animationFrameRequested());
@@ -45,11 +29,51 @@ window.addEventListener("load", function () {
 });
 
 function animationFrameRequested(timestamp) {
+
+    if (paused) {
+        return;
+    }
+
     renderer.render();
 
     setTimeout(() => {
-            requestAnimationFrame(() => animationFrameRequested());
-        }, 
+        requestAnimationFrame(() => animationFrameRequested());
+    },
         1000 / FPS
     );
+}
+
+function handlePause(key) {
+    if (key.key === " ") {
+        paused = !paused;
+        if (!paused) {
+            requestAnimationFrame(() => animationFrameRequested());
+        }
+    }
+}
+
+function handleDebug(key) {
+
+    // I'm making this mostly offline, we're probably not going to have this    
+    if (typeof SPECTOR === "undefined") {
+        console.error("SPECTOR has not been loaded correctly.");
+        return;
+    }
+
+    if (key.key !== "/") {
+        return;
+    }
+
+    const spector = new SPECTOR.Spector();
+    spector.spyCanvases();
+    spector.displayUI();
+
+    // NOTE: We have to render a frame to capture anything
+    renderer.render();
+
+    // Capture from canvas:
+    spector.captureCanvas(canvas).then((result) => {
+        const resultView = new SPECTOR.ResultView(spector);
+        resultView.display(result); // show the capture result
+    });
 }
