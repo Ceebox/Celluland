b99cee1171e5bde667fa1ff8363cfd33b73532dc
Ceebox <chadderboxdev@gmail.com>
2025-07-25 14:38:26 +0100

    Pre instanced drawing

---
+++ src/scripts/modules/automata/CellManager.mjs
@@ -0,0 +1,29 @@
+export class CellManager {
+
+    #cells = [];
+
+    constructor(rowCount, columnCount) {
+        this._rowCount = rowCount;
+        this._columnCount = columnCount;
+
+        // Initialize cells with default values
+        for (let i = 0; i < this._rowCount; i++) {
+            this.#cells[i] = [];
+            for (let j = 0; j < this._columnCount; j++) {
+                this.#cells[i][j] = 0;
+            }
+        }
+    }
+
+    updateCells(newStates) {
+        if (newStates.length !== this._rowCount || newStates[0].length !== this._columnCount) {
+            throw new Error("New states do not match the dimensions of the cell grid.");
+        }
+
+        for (let i = 0; i < this._rowCount; i++) {
+            for (let j = 0; j < this._columnCount; j++) {
+                this.#cells[i][j] = newStates[i][j];
+            }
+        }
+    }
+}
