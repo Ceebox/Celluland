5972a80a83c0a4ef79d98a89c175213d2abc0bee
Chadderbox <chadderboxdev@gmail.com>
2025-07-30 18:08:09 +0100

    Add colour api docs

--- scripts/modules/rendering/Colours.mjs
+++ scripts/modules/rendering/Colours.mjs
@@ -1,18 +1,18 @@
 export const COLOURS_ARRAY = [
-    { r: 0 / 255,   g: 0 / 255,   b: 0 / 255 },       // BLACK
+    { r: 0 / 255, g: 0 / 255, b: 0 / 255 },       // BLACK
     { r: 255 / 255, g: 255 / 255, b: 255 / 255 },     // WHITE
     { r: 128 / 255, g: 128 / 255, b: 128 / 255 },     // GRAY
-    { r: 255 / 255, g: 0 / 255,   b: 0 / 255 },       // RED
-    { r: 0 / 255,   g: 255 / 255, b: 0 / 255 },       // GREEN
-    { r: 0 / 255,   g: 0 / 255,   b: 255 / 255 },     // BLUE
+    { r: 255 / 255, g: 0 / 255, b: 0 / 255 },       // RED
+    { r: 0 / 255, g: 255 / 255, b: 0 / 255 },       // GREEN
+    { r: 0 / 255, g: 0 / 255, b: 255 / 255 },     // BLUE
     { r: 255 / 255, g: 255 / 255, b: 0 / 255 },       // YELLOW
     { r: 255 / 255, g: 165 / 255, b: 0 / 255 },       // ORANGE
-    { r: 165 / 255, g: 0 / 255,   b: 165 / 255 },     // PURPLE
-    { r: 0 / 255,   g: 255 / 255, b: 255 / 255 },     // CYAN
+    { r: 165 / 255, g: 0 / 255, b: 165 / 255 },     // PURPLE
+    { r: 0 / 255, g: 255 / 255, b: 255 / 255 },     // CYAN
     { r: 255 / 255, g: 192 / 255, b: 203 / 255 },     // PINK
-    { r: 165 / 255, g: 42 / 255,  b: 42 / 255 },      // BROWN
-    { r: 0 / 255,   g: 128 / 255, b: 0 / 255 },       // DARK_GREEN
-    { r: 0 / 255,   g: 0 / 255,   b: 128 / 255 },     // NAVY_BLUE
+    { r: 165 / 255, g: 42 / 255, b: 42 / 255 },      // BROWN
+    { r: 0 / 255, g: 128 / 255, b: 0 / 255 },       // DARK_GREEN
+    { r: 0 / 255, g: 0 / 255, b: 128 / 255 },     // NAVY_BLUE
 ];
 
 export const COLOURS = {
@@ -27,10 +27,9 @@ export const COLOURS = {
     PURPLE: 8,
     CYAN: 9,
     PINK: 10,
-    GRAY: 11,
-    BROWN: 12,
-    DARK_GREEN: 13,
-    NAVY_BLUE: 14,
+    BROWN: 11,
+    DARK_GREEN: 12,
+    NAVY_BLUE: 13,
 
     getColourIndex(colourString) {
         const index = this[colourString.toUpperCase()];
